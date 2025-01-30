9481b0f2f575b8664d6077b6d2af57aa091604c8
youyupei <youyupei@gmail.com>
2025-01-31 09:48:28 +1100

    feat: Update Rmarkdown files with cache and figure path options

--- rmarkdown/versioned_knit.R
+++ rmarkdown/versioned_knit.R
@@ -10,10 +10,14 @@ base_name <- tools::file_path_sans_ext(basename(input_file))
 version <- rmarkdown::yaml_front_matter(input_file)$version
 if (is.null(version)) {
     output_name <- paste0(base_name, "_", Sys.Date(), ".html")
+    cachedir <- paste0(base_name, "_", Sys.Date(), "_cache/")
+    fig.path <- paste0(base_name, "_", Sys.Date(), "_figures/")
 } else {
     output_name <- paste0(base_name, "_", version, "_", Sys.Date(), ".html")
+    cachedir <- paste0(base_name, "_", version, "_", Sys.Date(), "_cache/")
+    fig.path <- paste0(base_name, "_", version, "_", Sys.Date(), "_figures/")
 }
 
 # render the Rmarkdown file
 message("Rendering ", input_file, " to ", output_name)
-rmarkdown::render(input_file, output_file = output_name)
+rmarkdown::render(input_file, output_file = output_name, params = list(cache_dir = cachedir, fig.path = fig.path))
